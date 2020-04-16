33a8e51010b455a8898678672c6fc6f9e2a15a3e
Alexamannn <60818228+Alexamannn@users.noreply.github.com>
2020-04-17 01:28:11 +0200

    Update server.R

--- R/server.R
+++ R/server.R
@@ -1,11 +1,11 @@
 library(RSelenium)
 
 
-#remote_driver$open()
+connect <- function(input, output,session) {driver <- rsDriver(browser=c("firefox"))
+remote_driver <- driver[["client"]] }
 
 crawl <- function(input, output,session) {
- driver <- rsDriver(browser=c("firefox"))
-remote_driver <- driver[["client"]]
+ 
 remote_driver$navigate("https://tassidicambio.bancaditalia.it/timeSeries")
 Sys.sleep(10)
 date <- remote_driver$findElement(using = "xpath",  "//*[@id='dailyTab-panel']/div/form/div[1]/div")
